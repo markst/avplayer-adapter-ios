1b6e8ff38ff83463a1e7abb7bf3416cd2c3a4612
Mark Turner <turner.mark@abc.net.au>
2023-04-24 11:55:52 +1000

    Create `build_framework.sh` script.

---
+++ scripts/build_framework.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -e
+
+WORKSPACE="YouboraAVPlayerAdapter.xcworkspace"
+SCHEME="YouboraAVPlayerAdapter"
+FRAMEWORK_NAME="YouboraAVPlayerAdapter"
+
+BUILD_DIR="Build"
+TMP_DIR="${BUILD_DIR}/Tmp"
+IOS_ARCHIVE_PATH="${TMP_DIR}/iOS.xcarchive"
+IOS_SIM_ARCHIVE_PATH="${TMP_DIR}/iOSSimulator.xcarchive"
+
+rm -rf ${BUILD_DIR}
+rm -rf "${FRAMEWORK_NAME}.xcframework"
+
+xcodebuild archive \
+    -workspace "${WORKSPACE}" \
+    -scheme ${SCHEME} \
+    -archivePath ${IOS_SIM_ARCHIVE_PATH} \
+    -sdk iphonesimulator \
+    SKIP_INSTALL=NO
+
+xcodebuild archive \
+    -workspace "${WORKSPACE}" \
+    -scheme ${SCHEME} \
+    -archivePath ${IOS_ARCHIVE_PATH} \
+    -sdk iphoneos \
+    SKIP_INSTALL=NO
+
+xcodebuild -create-xcframework \
+    -framework ${IOS_SIM_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
+    -framework ${IOS_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
+    -output ${FRAMEWORK_NAME}.xcframework
+
+zip ${FRAMEWORK_NAME}.xcframework.zip ${FRAMEWORK_NAME}.xcframework
+
+rm -rf ${BUILD_DIR}
+
+swift package compute-checksum YouboraAVPlayerAdapter.xcframework.zip
